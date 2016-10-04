f9adfda0d0e1e3d17c8cf6261c2a001742f6a8fe
sunny.gulati <sunnywiz@gmail.com>
2016-10-03 19:17:22 -0700

    WIP: Adding EF and initial model/context

---
+++ MvcEFCodeFirstCRUD/MvcEFCodeFirstCRUD/Models/Models.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcEFCodeFirstCRUD.Models
+{
+    public class Planet 
+    {
+        public int PlanetID { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class PlanetContext : DbContext
+    {
+        public DbSet<Planet> Categories { get; set; }
+    }
+}
