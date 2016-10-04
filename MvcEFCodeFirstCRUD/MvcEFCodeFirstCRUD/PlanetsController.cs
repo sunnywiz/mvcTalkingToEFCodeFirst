0e57f673e173dfc00839f7fd93346f670451a719
sunny.gulati <sunnywiz@gmail.com>
2016-10-03 19:19:51 -0700

    Added Scaffolding Item

---
+++ MvcEFCodeFirstCRUD/MvcEFCodeFirstCRUD/PlanetsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcEFCodeFirstCRUD.Models;
+
+namespace MvcEFCodeFirstCRUD
+{
+    public class PlanetsController : Controller
+    {
+        private PlanetContext db = new PlanetContext();
+
+        // GET: Planets
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        // GET: Planets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Planet planet = db.Categories.Find(id);
+            if (planet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(planet);
+        }
+
+        // GET: Planets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Planets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PlanetID,Name")] Planet planet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(planet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(planet);
+        }
+
+        // GET: Planets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Planet planet = db.Categories.Find(id);
+            if (planet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(planet);
+        }
+
+        // POST: Planets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PlanetID,Name")] Planet planet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(planet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(planet);
+        }
+
+        // GET: Planets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Planet planet = db.Categories.Find(id);
+            if (planet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(planet);
+        }
+
+        // POST: Planets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Planet planet = db.Categories.Find(id);
+            db.Categories.Remove(planet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
